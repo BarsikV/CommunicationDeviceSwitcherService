6d33c8f3493f58b6c2c549da01d0bff29ed84f14
Valentyn <bars92@hotmail.it>
2020-05-16 11:35:33 +0300

    Changed install script bin path

--- Install service script.ps1
+++ Install service script.ps1
@@ -1,6 +1,6 @@
 $params = @{
     Name = "CommunicationAudioAutoSwitchService"
-    BinaryPathName = "C:\Users\Barsik\source\repos\AudioSwitcherService\AudioSwitcherService\bin\Release\netcoreapp3.1\AudioSwitcherService.exe"
+    BinaryPathName = "C:\CommunicationDeviceSwitcherService\CommunicationDeviceSwitcherService.exe"
     DisplayName = "Communication Audio Auto Switch Service"
     StartupType = "Automatic"
     Description = "This service automatically changes default communication output device, when the default playback device is changed"
