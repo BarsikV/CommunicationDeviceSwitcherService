0265317c9fc724f3ca0643f3f5494f348ce91354
BarsikV <bars92@hotmail.it>
2020-05-20 18:10:52 +0300

    Update Install service script.ps1
    updated the install script to use parameters

--- Install service script.ps1
+++ Install service script.ps1
@@ -1,9 +1,16 @@
+param (
+    $BinPath = "C:\CommunicationDeviceSwitcherService",
+    $Startup = "Automatic",
+    $ExeName = "\CommunicationDeviceSwitcherService.exe"
+)
+
 $params = @{
     Name = "CommunicationAudioAutoSwitchService"
-    BinaryPathName = "C:\CommunicationDeviceSwitcherService\CommunicationDeviceSwitcherService.exe"
+    BinaryPathName = "$BinPath$ExeName"
     DisplayName = "Communication Audio Auto Switch Service"
     StartupType = "Automatic"
     Description = "This service automatically changes default communication output device, when the default playback device is changed"
 }
 
 New-Service @params
+Start-Service $params.Name
