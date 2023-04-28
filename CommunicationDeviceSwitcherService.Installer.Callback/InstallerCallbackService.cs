93cb004f8946f771beb695a76f4737d7bfec8947
Valentyn Barsehian <valentyn.barsehian@3shape.com>
2023-04-28 15:16:51 +0200

    enable additional installation settings
    add .net 6 dependency to installer
    move installer callback to a separate project
    add an additional step during installation to select which type of devices needs to be switched

---
+++ CommunicationDeviceSwitcherService.Installer.Callback/InstallerCallbackService.cs
@@ -0,0 +1,57 @@
+﻿namespace CommunicationDeviceSwitcherService.Installer.Callback
+{
+    internal class InstallerCallbackService
+    {
+        private const string ServiceName = "CommunicationDeviceSwitcherService";
+        private const string ServiceName2 = "CommunicationAudioAutoSwitchService";
+        private const string Install = "/Install";
+        private const string Uninstall = "/Uninstall";
+
+        public static bool IsInstallerCallback(string[] args)
+        {
+            return args is { Length: > 0 } &&
+                (args[0] == Install ||
+                 args[0] == Uninstall);
+        }
+
+        public static async Task HandleInstallerCallback(string[] args)
+        {
+            var setupCommand = CallbackParametersReader.GetCallbackCommand(args);
+            var setupParameters = CallbackParametersReader.GetCallbackParameters(args);
+
+            switch (setupCommand)
+            {
+                case Install:
+                {
+                    await UninstallService();
+                    await InstallService(setupParameters);
+                    break;
+                }
+                case Uninstall:
+                {
+                    await UninstallService();
+                    break;
+                }
+            }
+        }
+
+        private static async Task InstallService(Dictionary<string, string?> setupParameters)
+        {
+            var executablePath = Path.Combine(AppContext.BaseDirectory, $"{ServiceName}.exe");
+
+            await WindowsServiceCommands.Create(ServiceName, executablePath);
+
+            await AppSettingsReadWriteService.UpdateAppsettingsJson(setupParameters);
+
+            await WindowsServiceCommands.Start(ServiceName);
+        }
+
+        private static async Task UninstallService()
+        {
+            await WindowsServiceCommands.Stop(ServiceName);
+            await WindowsServiceCommands.Stop(ServiceName2);
+            await WindowsServiceCommands.Delete(ServiceName);
+            await WindowsServiceCommands.Delete(ServiceName2);
+        }
+    }
+}
---
+++ CommunicationDeviceSwitcherService.Installer.Callback/Program.cs
@@ -0,0 +1,15 @@
+﻿using CommunicationDeviceSwitcherService.Installer.Callback;
+
+try
+{
+    if (!InstallerCallbackService.IsInstallerCallback(args))
+    {
+        return;
+    }
+
+    await InstallerCallbackService.HandleInstallerCallback(args);
+}
+catch (Exception e)
+{
+    await File.WriteAllTextAsync(Path.Combine(AppContext.BaseDirectory, "install_error.txt"), $"{e} \n {e.StackTrace}");
+}
